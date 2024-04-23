98ace2b7640028b94f6b4e3492ef81a5d1eacee3
RihardXXX <yusupa.akaeva@yandex.ru>
2024-04-23 20:21:13 +0300

    feat(...): add sass preprocessor

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,27 @@
 /** @type {import('next').NextConfig} */
 
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// Получаем путь к текущему файлу
+const __filename = fileURLToPath(import.meta.url);
+// Получаем директорию текущего файла
+const __dirname = dirname(__filename);
+
 
 const nextConfig = {
-    async redirects() {
-        return [
-            // Basic redirect
-            // {
-            //     source: '/authorization', // откуда
-            //     destination: '/', // куда
-            //     permanent: true,
-            // },
-        ]
+    // async redirects() {
+    //     return [
+    //         // Basic redirect
+    //         // {
+    //         //     source: '/authorization', // откуда
+    //         //     destination: '/', // куда
+    //         //     permanent: true,
+    //         // },
+    //     ]
+    // },
+    sassOptions: {
+        includePaths: [join(__dirname, 'styles')],
     },
 };
 
